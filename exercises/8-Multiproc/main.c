316e29c5a40a44542f71cc486cccaa3abf123de7
marnaa <47738897+marnaa@users.noreply.github.com>
2021-02-27 00:13:22 +0100

    almost done with exercise week 8, omp really slow, though

---
+++ exercises/8-Multiproc/main.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {double N; double count; unsigned int seed;} params; 
+
+void* function(void* arg){
+	params* p = (params*) arg;
+	double N = (*p).N;
+	double pse_count=0;
+	unsigned int seed = (*p).seed;
+	for(double i=0; i<=N; i+=1){
+		double x = (double) rand_r(&seed)/RAND_MAX;
+		double y = (double) rand_r(&seed)/RAND_MAX;
+		if(sqrt(pow(x,2)+pow(y,2))<=1){
+			pse_count+=1;
+		}
+		else{
+		}
+	}
+	p->count = pse_count;
+	return NULL;
+}
+
+int main(){
+	double N = 1e9;
+	double n1=N/3, n2=N/3, n3=N/3;
+	double calN= n1+n2+n3;
+	unsigned int seed1=0, seed2=0, seed3=0;
+	double count1 = 0, count2 = 0, count3 = 0;
+	pthread_t t1, t2, t3;
+	params p1 = {.N = n1, .count = count1, .seed = seed1};
+	params p2 = {.N = n2, .count = count2, .seed = seed2};
+	params p3 = {.N = n3, .count = count3, .seed = seed3};
+	pthread_create(&t1, NULL, function, (void*)&p1);
+	pthread_create(&t2, NULL, function, (void*)&p2);
+	pthread_create(&t3, NULL, function, (void*)&p3);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	pthread_join(t3,NULL);
+	double final_count = p1.count + p2.count + p3.count;
+	double est_pi = 4*final_count/(calN);
+	printf("est pi = %g and math pi = %g\n", est_pi, M_PI);
+	return 0;
+}
---
+++ exercises/8-Multiproc/ompmain.c
@@ -0,0 +1,57 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(){
+	double N = 1e9;
+	double n1=N/3, n2=N/3, n3=N/3;
+	double calN= n1+n2+n3;
+	unsigned int seed1=0, seed2=0, seed3=0;
+	double count1 = 0, count2 = 0, count3 = 0;
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		{
+			for(double i=0; i<=n1; i+=1){
+			double x = (double) rand_r(&seed1)/RAND_MAX;
+			double y = (double) rand_r(&seed1)/RAND_MAX;
+			if(sqrt(pow(x,2)+pow(y,2))<=1){
+				count1+=1;
+                	}
+                	else{
+                	}
+			}
+		}
+		#pragma omp section
+		{
+			for(double i=0; i<=n2; i+=1){
+				double x = (double) rand_r(&seed2)/RAND_MAX;
+				double y = (double) rand_r(&seed2)/RAND_MAX;
+				if(sqrt(pow(x,2)+pow(y,2))<=1){
+					count2+=1;
+                		}
+                		else{
+                		}
+       			}
+		}
+		#pragma omp section
+		{
+			for(double i=0; i<=n3; i+=1){
+			double x = (double) rand_r(&seed3)/RAND_MAX;
+			double y = (double) rand_r(&seed3)/RAND_MAX;
+			if(sqrt(pow(x,2)+pow(y,2))<=1){
+				count3+=1;
+                	}
+                	else{
+                	}
+       		}
+		}
+
+
+	}
+	double final_count = count1 + count2 + count3;
+	double est_pi = 4*final_count/(calN);
+	printf("est pi = %g and math pi = %g\n", est_pi, M_PI);
+	return 0;
+}
