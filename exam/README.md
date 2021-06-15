f7c12353b5efba56530e54d99d26d0ac9cccf14d
marnaa <47738897+marnaa@users.noreply.github.com>
2021-06-15 10:21:35 +0200

    Exam project initialized

---
+++ exam/README.md
@@ -0,0 +1,1 @@
+Besvarelse til praktisk programmerings eksamen eksamensprojekt 2 (Lanczos algoritme)
---
+++ exam/main.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	return 0;
+}
